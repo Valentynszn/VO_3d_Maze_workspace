f0a9bba9f85833618147cb38c1b1836ce79373ae
Oseni Matthew <osenimatthew9@gmail.com>
2024-06-17 13:50:34 +0100

    window and a dot at the center

--- Oseni/src/init.c
+++ Oseni/src/init.c
@@ -13,16 +13,21 @@ int main(void)
 	SDL_Renderer *renderer = NULL;
 
 	SDL_Init(SDL_INIT_VIDEO);
-	SDL_CreateWindowAndRenderer(640, 480, 0, &window, &renderer);
+	if (SDL_CreateWindowAndRenderer(640, 480, 0, &window, &renderer) < 0);
+	{
+		printf("Window and renderer could not be created! SDL_Error: %s\n", SDL_GetError());
+		SDL_Quit();
+		return (1);
+	}
 
 	SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255); /*Set draw colour to black*/
 	SDL_RenderClear(renderer); /*Clear the screen with black*/
 
-	SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255); /*Set draw color to red*/
+	SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255); /*Set draw color to white*/
 	SDL_RenderDrawPoint(
 			renderer,
 		       640 / 2,
-		       480 / 2); /*Draw a red point at the center of the screen*/
+		       480 / 2); /*Draw a white point at the center of the screen*/
 
 	SDL_RenderPresent(renderer); /*update the screen*/
 

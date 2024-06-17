115d0712032b888d1b89a7adb79649e15ea96060
Oseni Matthew <osenimatthew9@gmail.com>
2024-06-17 14:22:43 +0100

    window and a dot at the center

--- Oseni/src/init.c
+++ Oseni/src/init.c
@@ -12,11 +12,26 @@ int main(void)
 	SDL_Window *window = NULL;
 	SDL_Renderer *renderer = NULL;
 
-	SDL_Init(SDL_INIT_VIDEO);
-	SDL_CreateWindow("window",
-			SDL_WINDOWPOS_UNDEFINED,
-			SDL_WINDOWPOS_UNDEFINED,
-			640, 480, SDL_WINDOW_SHOWN);
+	if (SDL_Init(SDL_INIT_VIDEO) < 0);
+	{
+		printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
+		return (1);
+	}
+
+	SDL_CreateWindow(
+			"window",
+			SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
+			640, 480,
+			SDL_WINDOW_SHOWN
+			);
+
+	if (window == NULL)
+	{
+		 printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
+		 SDL_Quit();
+		 return (1);
+	}
+
 	SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
 	if (renderer == NULL)
 	{

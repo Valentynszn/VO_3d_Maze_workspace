bcf8a7c2247a2986ae50c5f041cd8214b0ead0af
Oseni Matthew <osenimatthew9@gmail.com>
2024-06-22 12:25:33 +0100

    grid

--- Oseni/new_src/grid.c
+++ Oseni/new_src/grid.c
@@ -12,7 +12,7 @@
 
 void renderMap(SDL_Renderer *renderer, char map[MAP_HEIGHT][MAP_WIDTH]);
 bool checkCollision(SDL_Rect *bot, char map[MAP_HEIGHT][MAP_WIDTH]);
-
+void renderGrid(SDL_Renderer *renderer, int windowWidth, int windowHeight, int tileSize);
 
 /**
  * Main function to initialize SDL, create a window and renderer, draw a point in the center of the screen..
@@ -38,7 +38,7 @@ int main(void)
 
 	window = SDL_CreateWindow(
 			"tutorial",
-			SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
+			SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
 			1280, 600,
 			SDL_WINDOW_SHOWN
 			);
@@ -60,6 +60,21 @@ int main(void)
 	{6, 0, 0, 0, 6, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 6},
 	{6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6},
 };
+
+void renderGrid(SDL_Renderer *renderer, int windowWidth, int windowHeight, int tileSize)
+{
+	SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+
+	for (int x = 0; x < windowWidth; x += tileSize)
+	{
+		SDL_RenderDrawLine(renderer, x, 0, x, windowHeight);
+	}
+
+	for (int y = 0; y < windowHeight; y += tileSize)
+	{
+		SDL_RenderDrawLine(renderer, 0, y, windowWidth, y);
+	}
+}
 
 void renderMap(SDL_Renderer *renderer, char map[MAP_HEIGHT][MAP_WIDTH])
 {
@@ -159,13 +174,14 @@ bool checkCollision(SDL_Rect *bot, char map[MAP_HEIGHT][MAP_WIDTH])
 			angle -= 2 * M_PI;
 
 	SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255); /*Set draw colour to black*/
+	renderGrid(renderer, 1280, 600, TILE_SIZE);
 	SDL_RenderClear(renderer); /*Clear the screen with black*/
 	renderMap(renderer, map);
 
 	int centerX = 1280 / 2;
 	int centerY = 600 / 2;
 	SDL_Rect bot = {SCREEN_WIDTH / 2 - bot_size / 2, SCREEN_HEIGHT / 2 - bot_size / 2, bot_size, bot_size}; /* Start at the center of the screen */
-	SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255); /*Set draw color to red*/
+	SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255); /*Set draw color to white*/
 	SDL_RenderFillRect(renderer, &bot);
 
 	/*Calculate the center of the bot*/
@@ -177,7 +193,7 @@ bool checkCollision(SDL_Rect *bot, char map[MAP_HEIGHT][MAP_WIDTH])
         int lineEndY = botCenterY + (bot_size / 2) * sin(angle);
 
 	/*Set the draw color to white and draw the line*/
-	SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255); // White color
+	SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255); /*red color*/
 	SDL_RenderDrawLine(renderer, botCenterX, botCenterY, lineEndX, lineEndY);
 
 
